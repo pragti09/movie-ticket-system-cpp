d68b1c42601053792b4e8c3d3df0e3172671ee2f
Kiran-1627 <kkamritsar2005@gmail.com>
2025-10-29 16:20:26 +0530

    Deletemovie code

---
+++ movie.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class movie{
+    public:
+    string title;
+    string timings;
+    int seats;
+    int price;
+};
+class Deletemovie{
+    movie movies[100];
+    int c=5;
+    public:
+    void dummy(){
+       movies[0] = {"interstella","10:00 AM",150,20};
+       movies[1] = {"nutella","2:00 PM",200,40};
+       movies[2] = {"shell","10:00 AM",159,50};
+       movies[3] = {"beautiful","4:00 PM",300,10};
+       movies[4] = {"nun","5:00PM",250,50};
+
+    }
+    void delMovie(string name){
+        for(int i=0;i<c;i++){
+            if(movies[i].title==name)
+            {
+                for(int j=i;j<c-1;j++){
+                    movies[j].title=movies[j+1].title;
+                }
+                c--;
+                cout<<"Deleted Successfully"<<endl;
+                return;
+            }
+        }
+        cout<<"Movie not found"<<name<<endl;
+    }
+    void show()
+        {
+            for(int i=0;i<c;i++){
+                cout<<"Movies: "<< movies[i].title<<" "<<movies[i].timings<<" "<<movies[i].seats<<" "<<movies[i].price<<endl;
+            }
+        }
+
+
+
+    };
